c231c26d2a63adaef059187784a65501404565d1
AlexPolyakovsky <alexander@appcharge.com>
2023-06-13 13:31:24 +0300

    updated the model with price

--- src/controllers/player/Models.cs
+++ src/controllers/player/Models.cs
@@ -12,6 +12,12 @@ public class Product
     public string name { get; set; } = default!;
 }
 
+public class Price
+{
+    public float subTotal { get; set; } = default!;
+    public float tax { get; set; } = default!;
+}
+
 public class PublisherPayload
 {
     public string appChargePaymentId { get; set; } = default!;
@@ -24,6 +30,7 @@ public class PublisherPayload
     public int priceInCents { get; set; } = default!;
     public string currency { get; set; } = default!;
     public float priceInDollar { get; set; } = default!;
+    public Price price { get; set; } = default!;
     public string action { get; set; } = default!;
     public string actionStatus { get; set; } = default!;
     public List<Product> products { get; set; } = default!;
